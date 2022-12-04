212f0ff13591a263753dbc9b5e4962deaa61d1b8
Senis John <thank243@gmail.com>
2022-12-04 08:43:10 +0800

    fix: log prefix display not completed

--- common/limiter/errors.go
+++ common/limiter/errors.go
@@ -2,8 +2,6 @@ package limiter
 
 import "github.com/xtls/xray-core/common/errors"
 
-type errPathObjHolder struct{}
-
 func newError(values ...interface{}) *errors.Error {
-	return errors.New(values...).WithPathObj(errPathObjHolder{})
+	return errors.New(values...)
 }
