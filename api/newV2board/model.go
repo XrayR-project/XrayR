8060dfc183daf4c1445c36498f11acdb74847e47
betaxab <betaxab@gmail.com>
2023-11-28 04:51:55 +0800

    api: newV2board: add Vless support

--- api/newV2board/model.go
+++ api/newV2board/model.go
@@ -35,6 +35,21 @@ type v2ray struct {
 		ServiceName string           `json:"serviceName"`
 		Header      *json.RawMessage `json:"header"`
 	} `json:"networkSettings"`
+	VlessNetworkSettings struct {
+		Path        string           `json:"path"`
+		Headers     *json.RawMessage `json:"headers"`
+		ServiceName string           `json:"serviceName"`
+		Header      *json.RawMessage `json:"header"`
+	} `json:"network_settings"`
+	VlessFlow   string `json:"flow"`
+	VlessTlsSettings struct {
+		ServerPort string `json:"server_port"`
+		Dest       string `json:"dest"`
+		xVer       uint64 `json:"xver"`
+		Sni        string `json:"server_name"`
+		PrivateKey string `json:"private_key"`
+		ShortId    string `json:"short_id"`
+	} `json:"tls_settings"`
 	Tls int `json:"tls"`
 }
 
