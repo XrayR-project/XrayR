ce7cd0de30e381b4d67b4f2b2afb5012d9d267f3
Jat <jat@sinosky.org>
2024-01-09 02:00:18 +0800

    support passing redis network and redis username to redis client
    Signed-off-by: Jat <jat@sinosky.org>

--- common/limiter/model.go
+++ common/limiter/model.go
@@ -2,7 +2,9 @@ package limiter
 
 type GlobalDeviceLimitConfig struct {
 	Enable        bool   `mapstructure:"Enable"`
-	RedisAddr     string `mapstructure:"RedisAddr"` // host:port
+	RedisNetwork  string `mapstructure:"RedisNetwork"` // tcp or unix
+	RedisAddr     string `mapstructure:"RedisAddr"`    // host:port, or /path/to/unix.sock
+	RedisUsername string `mapstructure:"RedisUsername"`
 	RedisPassword string `mapstructure:"RedisPassword"`
 	RedisDB       int    `mapstructure:"RedisDB"`
 	Timeout       int    `mapstructure:"Timeout"`
