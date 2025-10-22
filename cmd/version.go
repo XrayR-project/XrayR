cd6708511171ffaa015def27fbd81e5ad367663f
MingLoong <52956168+MingLoong@users.noreply.github.com>
2025-10-22 13:10:42 +0800

    Update version.go

--- cmd/version.go
+++ cmd/version.go
@@ -1,8 +1,27 @@
-// cmd/version.go
 package cmd
 
-import "fmt"
+import (
+	"fmt"
 
-func showVersion() { // 小写首字母
-	fmt.Println("XrayR 0.9.5 (A Xray backend that supports many panels)")
+	"github.com/spf13/cobra"
+)
+
+var (
+	version  = "0.9.5"
+	codename = "XrayR"
+	intro    = "A Xray backend that supports many panels"
+)
+
+func init() {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "Print current version of XrayR",
+		Run: func(cmd *cobra.Command, args []string) {
+			showVersion()
+		},
+	})
+}
+
+func showVersion() {
+	fmt.Printf("%s %s (%s) \n", codename, version, intro)
 }
